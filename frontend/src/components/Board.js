ce6f416f6f7eb90691f07e070053105470facf8d
Cameron Mah <emilsdespair@gmail.com>
2023-01-19 20:30:00 -0500

    Timer, Small board changes
    - Added timer
    - Small board changes

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,13 +1,17 @@
 import { Chessboard } from "react-chessboard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Chess } from "chess.js";
 import moveAudio from "../media/move-self.mp3"
 import captureAudio from "../media/capture.mp3"
 import { useLocation } from "react-router-dom";
 
+
+
 import { Typography, List, ListItemText, Box, TextField } from "@mui/material";
 
+
 import io from 'socket.io-client'
+import { width } from "@mui/system";
 //const socket = io.connect("http://localhost:3001")
 const socket = io.connect("https://chess-rooms-app.onrender.com")
 
@@ -16,13 +20,16 @@ const socket = io.connect("https://chess-rooms-app.onrender.com")
 const Board = () =>{
     const [chatLog, setChatLog] = useState([])
     const [game, setGame] = useState(new Chess());
+    const turn = useRef(false)
     const [opponent, setOpponent] = useState("");
     const location = useLocation();
     let {roomName} = location.state;
     let {name} = location.state;
-    const [color, setColor] = useState("Waiting for opponent...")
+    const [color, setColor] = useState("")
     const moveSound = new Audio(moveAudio);
     const captureSound = new Audio(captureAudio);
+    const [mySeconds, setMySeconds] = useState(600)
+    const [myTime, setMyTime] = useState("")
 
     const sendBoard = (data) =>{
         socket.emit("send_move", data)
@@ -52,6 +59,8 @@ const Board = () =>{
                 to: targetSquare,
             });
             if(res){
+                turn.current = false
+                console.log(turn.current)
                 if(res.captured){
                     captureSound.play()
                 }
@@ -73,9 +82,39 @@ const Board = () =>{
     }
 
     useEffect(()=>{
+        const interval = setInterval(()=>{
+            if(turn.current){
+                //console.log(turn.current)
+                setMySeconds(mySeconds => mySeconds - 1)
+            }
+            console.log(game)
+        }, 1000)
+        return () =>{
+            clearInterval(interval)
+        }
+    }, [color], [game])
+
+    useEffect(()=>{
         socket.emit("set_nickname", name)
         joinRoom()
     }, [])
+
+    //Not sure why I couldn't put it after setMySeconds above...
+    useEffect(()=>{
+        //Formatting time
+        if(Math.floor(mySeconds/60) < 10 && mySeconds%60 < 10){
+            setMyTime("0"+Math.floor(mySeconds/60).toString()+" : 0"+(mySeconds%60).toString())
+        }
+        else if(mySeconds%60 < 10){
+            setMyTime(Math.floor(mySeconds/60).toString()+" : 0"+(mySeconds%60).toString())
+        }   
+        else if(Math.floor(mySeconds/60) < 10){
+            setMyTime("0"+Math.floor(mySeconds/60).toString()+" : "+(mySeconds%60).toString())
+        }
+        else{
+            setMyTime(Math.floor(mySeconds/60).toString()+" : "+(mySeconds%60).toString())
+        }
+    },[mySeconds])
     
 
     useEffect(()=> {
@@ -97,20 +136,24 @@ const Board = () =>{
             const gameCopy = new Chess();
             gameCopy.loadPgn(data);
             setGame(gameCopy)
+            turn.current = true
             console.log("recieved")
         })
         socket.on("recieve_color", (col) =>{
-            setColor(col)
-            //console.log(col)
+            setColor(color => col)
+            if(col == "white"){
+                turn.current = true
+            }
         })
         socket.on("get_opponent", (oppName) =>{
-            setOpponent(oppName)
+            setOpponent(opponent => oppName)
         })
         socket.on("recieve_chat", (s)=>{
             setChatLog(chatLog =>[...chatLog, s])
         })
     }, [socket])
 
+    
     return (
     <>
         <Typography align="right" variant = "h4" marginRight={30}>Room: {roomName}</Typography>
@@ -118,7 +161,16 @@ const Board = () =>{
             <div style = {{alignItems: 'center', marginLeft: '25%',justifyContent: 'center', width: '80vh'}}>
             <Typography variant = "h4">{opponent}</Typography>
             <Chessboard position={game.fen()} onPieceDrop={onDrop} id="BasicBoard" boardOrientation={color}/>
-            <Typography variant = "h4">{name}</Typography>
+            <div style={{height: '100%', width: '100%'}}>
+                <div style = {{marginTop: '1vw', display: 'inline-block', flexDirection: 'row', float: 'left'}}>
+                    <Typography variant = "h4" align="left">{name}</Typography>
+                </div>
+                <div style = {{marginTop: '1vw', display: 'inline-block', flexDirection: 'row', float: 'right'}}>
+                    <Typography variant = "h4" align="right">{myTime}</Typography>
+                </div>
+            </div>
+            
+            
             </div>
             <Box sx={{border: '1px solid grey', width: '20%', marginLeft: '5%', position: "relative", maxHeight: '70vh', marginTop: '5%'}}>
                 <Typography variant = "h4" align="center">Chat</Typography>
